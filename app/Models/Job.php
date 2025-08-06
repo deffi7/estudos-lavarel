46a0aac001dded35fd14506e8ea51805a38e9887
João Pedro <deffizin@gmail.com>
2025-08-06 11:56:51 -0300

    Utilizando factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,11 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Arr;
 
-class Job extends Model {
- protected $table = 'job_listings';
+class Job extends Model
+{
+    use HasFactory;
 
- protected $fillable = ['title', 'salary'];
+    protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
 }
 
