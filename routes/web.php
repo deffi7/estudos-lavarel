31547b948f10d56a3f5d4dc78c7854a588534f8d
João Pedro <deffizin@gmail.com>
2025-08-13 11:47:22 -0300

    Edit Route and Controller

--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,10 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
-use App\Models\Job;
 
-
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->paginate(5);
-
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-
-    return view('jobs.show', ['job' => $job]);
-
-});
-
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-Route::get('/contato', function () {
-    return view('contato');
-});
-
+Route::view('/', 'home');
+Route::resource('jobs', JobController::class);
+Route::view('/contato', 'contato');
 Route::resource('produtos', ProdutoController::class);
